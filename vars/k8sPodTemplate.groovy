e4c0672a2ec7d35f50d0175617e07ed91330f8c7
shinGyeongseon <tlsrid1119@gmail.com>
2025-03-29 22:38:44 +0900

    refactor template builder

--- vars/k8sPodTemplate.groovy
+++ vars/k8sPodTemplate.groovy
@@ -4,7 +4,6 @@ import org.shin.example.jenkins.K8sPodTemplateBuilder
 def call(Map params = [:]) {
     // 기본 템플릿은 항상 포함
     def resource = libraryResource('podTemplates/base.yaml')
-    resource += libraryResource('podTemplates/kaniko.yaml')
 
     if (params.containsKey('kaniko')) {
         resource += libraryResource('podTemplates/kaniko.yaml')
