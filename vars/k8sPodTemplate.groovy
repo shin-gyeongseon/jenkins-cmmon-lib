d72572de851261e57099d2cc361b6e5a8e9886d2
shinGyeongseon <tlsrid1119@gmail.com>
2025-03-29 22:28:28 +0900

    refactor template builder

--- vars/k8sPodTemplate.groovy
+++ vars/k8sPodTemplate.groovy
@@ -5,18 +5,14 @@ def call(Map params = [:]) {
     // 기본 템플릿은 항상 포함
     def resource = libraryResource('podTemplates/base.yaml')
 
-    // 1. Map 형태로 파라미터가 전달된 경우
-    if (params instanceof Map) {
-        // 기존 방식대로 키 존재 여부로 템플릿 추가
-        if (params.containsKey('kaniko')) {
-            resource = resource + '\n' + libraryResource('podTemplates/kaniko.yaml')
-        }
-        if (params.containsKey('kubectl')) {
-            resource = resource + '\n' + libraryResource('podTemplates/kubectl.yaml')
-        }
-        if (params.containsKey('argocd')) {
-            resource = resource + '\n' + libraryResource('podTemplates/argocd.yaml')
-        }
+    if (params.containsKey('kaniko')) {
+        resource = resource + '\n' + libraryResource('podTemplates/kaniko.yaml')
+    }
+    if (params.containsKey('kubectl')) {
+        resource = resource + '\n' + libraryResource('podTemplates/kubectl.yaml')
+    }
+    if (params.containsKey('argocd')) {
+        resource = resource + '\n' + libraryResource('podTemplates/argocd.yaml')
     }
 
     return resource
