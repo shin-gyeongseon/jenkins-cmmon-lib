c2ac7b1c9b71cb6a89f8285683dd389f3d722183
shinGyeongseon <tlsrid1119@gmail.com>
2025-03-29 21:32:23 +0900

    refactor vars

--- vars/k8sPodTemplate.groovy
+++ vars/k8sPodTemplate.groovy
@@ -1,20 +1,6 @@
 #!/usr/bin/env groovy
 
-import org.shin.example.jenkins.K8sPodTemplateBuilder
-
 def call(Map params = [:]) {
-    def builder = new K8sPodTemplateBuilder()
-
-    String name = params.get('name', 'default')
-    String label = params.get('label', "${name}-${UUID.randomUUID().toString()}")
-    String cloud = params.get('cloud', 'kubernetes')
-
-    // Pod 템플릿 생성
-    def podYaml = builder.buildPodTemplate(params)
-
-    return [
-            cloud: cloud,
-            label: label,
-            yaml: podYaml
-    ]
+    def resource = libraryResource('podTemplates/base.yaml')
+    return resource
 }
